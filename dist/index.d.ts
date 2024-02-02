d37fa18f0a2c39b945ee4202c5d92ba12e42495b
Kilian Brachtendorf <kilian.brachtendorf@adac-travelevent.de>
2024-02-02 07:02:00 +0100

    include new type definition file

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,40 +1,53 @@
-import Elysia, { PreContext } from 'elysia';
+import Elysia, { PreContext } from 'elysia'
 export type BasicAuthCredentials = {
-    username: string;
-    password: string;
-};
-export type BasicAuthCredentialOptions = {
-    env: string;
-} | {
-    file: string;
-} | BasicAuthCredentials[];
-export type BasicAuthOptions = {
-    enabled: Boolean;
-    credentials: BasicAuthCredentialOptions;
-    header: string;
-    realm: string;
-    unauthorizedMessage: string;
-    unauthorizedStatus: number;
-    scope: string | string[] | ((ctx: PreContext) => boolean);
-    skipCorsPreflight: boolean;
-};
-export declare class BasicAuthError extends Error {
-    readonly message: string;
-    readonly realm: string;
-    code: string;
-    constructor(message: string, realm: string);
+  username: string
+  password: string
 }
-export declare function basicAuth(userOptions?: Partial<BasicAuthOptions>): Elysia<"", {
-    request: {};
+export type BasicAuthCredentialOptions =
+  | {
+      env: string
+    }
+  | {
+      file: string
+    }
+  | BasicAuthCredentials[]
+export type BasicAuthOptions = {
+  enabled: Boolean
+  credentials: BasicAuthCredentialOptions
+  header: string
+  realm: string
+  unauthorizedMessage: string
+  unauthorizedStatus: number
+  scope: string | string[] | ((ctx: PreContext) => boolean)
+  skipCorsPreflight: boolean
+}
+export declare class BasicAuthError extends Error {
+  readonly message: string
+  readonly realm: string
+  code: string
+  constructor(message: string, realm: string)
+}
+export declare function basicAuth(
+  userOptions?: Partial<BasicAuthOptions>
+): Elysia<
+  '',
+  {
+    request: {}
     store: {
-        basicAuthRealm: string | null;
-        basicAuthUser: string | null;
-    };
-    derive: {};
-    resolve: {};
-}, {
-    type: {};
+      basicAuthRealm: string | null
+      basicAuthUser: string | null
+    }
+    derive: {}
+    resolve: {}
+  },
+  {
+    type: {}
     error: {
-        readonly BASIC_AUTH_ERROR: BasicAuthError;
-    };
-}, {}, {}, {}, false>;
+      readonly BASIC_AUTH_ERROR: BasicAuthError
+    }
+  },
+  {},
+  {},
+  {},
+  false
+>
